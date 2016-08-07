f2e28367448a30dba8c3e7b7e76df426e1549006
chanmoo13 <chanmoo13@snu.ac.kr>
2016-08-07 16:20:39 +0900

    Example

--- Examples/Ex1_Matrix_Transpose.R
+++ Examples/Ex1_Matrix_Transpose.R
@@ -1,14 +1,38 @@
 library(Rcpp)
-
+library(rbenchmark)
 
 #### Example 1 : Matrix Transpose Using Rcpp ####
 
 
 sourceCpp("transposeRcpp.cpp")
 
-mat<-c(1,2,3,4,5,6,7,8,9,10,11,12)
-dim(mat)<-c(3,4)
+mat<-rep(c(1,2,3,4,5,6,7,8,9,10,11,12),100)
+dim(mat)<-c(30,40)
 mat
 
 tmat<-transposeC(mat)
 tmat
+
+
+
+
+#### Benchmark Comparison with          ####
+#### R built-in function "t" and        ####
+#### R user-made function "transposeR"  ####
+
+transposeR <- function(x){  ### User-made transpose function ( same algorithm with "transposeC" )
+  n<-nrow(x)
+  m<-ncol(x)
+  tx<-matrix(nrow=m,ncol=n)
+  for(i in 1:m){
+    for(j in 1:n){
+      tx[i,j] = x[j,i]
+    }
+  }
+  return(tx)
+}
+
+benchmark(transposeC(mat),t(mat), transposeR(mat),
+          columns=c("test", "replications", "elapsed", "relative"), order="relative", replications = 10000)
+
+
